def16e36492ea0df5920e7700ebe5119e3bebc40
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-07 22:22:11 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,3 +1,16 @@
+const shape = require("./shape");
+
+// declare class
+class Circle extends shape {
+  constructor() {
+    super();
+    this.color = shape.color;
+  }
+  calculateArea() {}
+}
+
+// export class using module.exports
+module.exports = Circle;
 // import using require
 
 // declare class
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
+class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+
+// export class using module.exports
+module.exports = Shape;
 // declare class
 
 // export class using module.exports
